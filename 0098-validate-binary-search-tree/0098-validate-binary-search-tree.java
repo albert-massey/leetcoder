c1f2d0f2c620de8667db03fe818f14ceddc9f282
Albert Massey <eralbert9191@gmail.com>
2024-01-20 17:54:58 -0800

    Time: 0 ms (100.00%), Space: 43.1 MB (85.71%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean flag = true;
+    TreeNode prev;
+    public boolean isValidBST(TreeNode root) {
+        inorder(root);
+        return flag;
+    }
+
+    private void inorder (TreeNode root) {
+        //base
+        if(root == null) return;
+
+        //logic
+        inorder(root.left);
+        // TreeNode curr = root;
+        if (prev != null && prev.val >= root.val) {
+            flag = false;
+        }
+        prev = root;
+        inorder(root.right);
+    } 
+}
