30697b7d9d7ce1f8faefa37310a6d8c539a4fc52
Albert Massey <eralbert9191@gmail.com>
2024-01-25 15:23:03 -0800

    Time: 143 ms (64.61%), Space: 41.6 MB (43.60%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,6 +2,7 @@ class Solution {
     boolean found = false;
     int[][] dirs = {{0,1}, {0, -1}, {-1, 0}, {1, 0}};
     public boolean exist(char[][] board, String word) {
+        if(board == null || board.length == 0) return false;
         int m = board.length;
         int n = board[0].length;
         for (int i = 0; i < m; i++) {
