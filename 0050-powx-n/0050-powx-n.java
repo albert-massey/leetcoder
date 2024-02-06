71f02fd6953f9fb494111465c431c5426b2ca335
Albert Massey <eralbert9191@gmail.com>
2024-02-05 16:48:42 -0800

    Time: 0 ms (100.00%), Space: 42.3 MB (28.06%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        //base
+        if(n == 0) return 1.0;
+            
+        //logic
+        double result = myPow(x, Math.abs(n/2));
+        if(n % 2 == 0) {
+            result = result * result;
+        } else {
+            result = result * result * x;
+        }
+        
+        if (n < 0) return 1/result;
+        return result;
+    }
+}
