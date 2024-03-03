f253627ff67652ed6a271f4360252660015cb08e
Albert Massey <eralbert9191@gmail.com>
2024-03-02 16:00:28 -0800

    Time: 597 ms (6.16%), Space: 46.8 MB (6.84%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,22 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        if(nums.length == 1) return true;  
         int n = nums.length;
-        int target = n -1;
-        for (int i = n -2; i >=0; i--) {
-            int curr = nums[i];
-            if(curr + i >= target) {
-                target = i;
+        Queue<Integer> q = new LinkedList<>();
+        HashSet<Integer> set = new HashSet<>();
+        q.add(0);
+        set.add(0);
+        while(!q.isEmpty()) {// 2 3 0 1 4
+            int currIdx = q.poll(); // 0
+            for(int i = 1; i <= nums[currIdx]; i++) {
+                int newIdx = currIdx + i;
+                if(newIdx >= n -1)  return true;
+                if(!set.contains(newIdx)){
+                    q.add(newIdx);
+                    set.add(newIdx);
+                }
             }
         }
-        return target == 0;
+        return false;
     }
 }
