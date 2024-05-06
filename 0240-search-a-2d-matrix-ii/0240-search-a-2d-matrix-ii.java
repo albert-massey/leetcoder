5a6bf6857b5285c6c2b530e0e37a54ccced08653
Albert Massey <eralbert9191@gmail.com>
2024-05-06 16:50:07 -0700

    Time: 5 ms (99.94%), Space: 45.4 MB (89.70%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 1 && matrix[0].length == 1) return target == matrix[0][0];
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int r = 0;
+        int c = n -1;
+        while (r >= 0 && c >= 0 && r <= m -1 && c <= n -1) {
+            if (matrix[r][c] == target) {
+                return true;
+            } else if(matrix[r][c] < target) {
+                r++;
+            } else if(matrix[r][c] > target) {
+                c--;
+            }
+        }
+        return false;
+    }
+}
