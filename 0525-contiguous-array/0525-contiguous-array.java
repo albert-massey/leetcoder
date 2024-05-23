d18751a7e8d5f0e0a90bc573522218566d9b38c5
Albert Massey <eralbert9191@gmail.com>
2024-05-23 16:58:10 -0700

    Time: 24 ms (56.50%), Space: 55.8 MB (5.50%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findMaxLength(int[] nums) {
         if(nums.length == 1) return 0;
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         int[] rSum = new int[nums.length];
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
@@ -14,8 +14,7 @@ class Solution {
             }
             if(!map.containsKey(sum)) {
                 map.put(sum, i);
-            }
-            if(map.containsKey(sum)) {
+            } else {
                 int diff = i - map.get(sum);
                 max = Math.max(max, diff);
             }
