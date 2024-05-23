8bd74997f610aae6a752867fd624602a8d234e6a
Albert Massey <eralbert9191@gmail.com>
2024-05-22 17:29:03 -0700

    Time: 0 ms (100.00%), Space: 41 MB (45.91%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public int sumNumbers(TreeNode root) {
+//         int sum = 0;
+//         int currNum = root.val;
+//         helper(root, sum, currNum);
+//         return sum;
+//     }
+    
+//     private int helper(TreeNode root, int sum, int currNum) {
+//         //base
+//         if(root == null) return 0;
+        
+//         //logic
+//         helper(root.left, sum, currNum);
+        
+//         helper(root.right, sum, currNum);
+        
+        
+//     }
+// }
+class Solution {
+    int globalSum = 0;
+    public int sumNumbers(TreeNode root) {
+        inorder(root, 0);
+        return globalSum;
+    }
+    private void inorder(TreeNode root, int currNum) {
+        //
+        if(root == null) return;
+
+        //
+        currNum = currNum * 10 + root.val;
+        inorder(root.left, currNum);
+        if(root.left == null && root.right == null) {
+            globalSum += currNum;
+        }
+        inorder(root.right, currNum);
+    }
+}
