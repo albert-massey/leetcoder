9148f19e60d617fd57c88a8a3b8c9142c2d1709e
Albert Massey <eralbert9191@gmail.com>
2024-06-04 19:52:40 -0700

    Time: 4 ms (41.46%), Space: 49.7 MB (30.82%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,70 @@
+class Solution {
+    int[][] dirs = new int[][] { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
+
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    public void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] != '1') {
+            return;
+        }
+
+        // visited
+        grid[row][col] = '0';
+
+        for (int[] dir : dirs) {
+            dfs(grid, row + dir[0], col + dir[1]);
+        }
+    }
+}
+
+
+
+// class Solution {
+//     char[][] grider;
+//     int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+//     public int numIslands(char[][] grid) {
+//         this.grider = grid;
+//         int count = 1;
+//         int m = grid.length;
+//         int n = grid[0].length;
+        
+//         for(int i = 0; i < m; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 char c = grid[i][j];
+//                 if(c == '1') {
+//                     count++;
+//                     dfs(grider, i, j, count);
+//                 }
+//             }
+//         }
+//         return count - 1;
+//     }
+    
+//     private void dfs(char[][] grider, int i, int j, int count) {
+//         //base
+//         if(i > grider.length - 1
+//            || i < 0 
+//            || j > grider[0].length - 1 
+//            || j < 0 
+//            || grider[i][j] != '1') return;
+        
+//         //logic
+//         grider[i][j] = (char)count;
+//         for(int[] dir: dirs) {
+//             int nr = dir[0] + i;
+//             int nc = dir[1] + j;
+//             dfs(grider, nr, nc, count);
+//         }
+//     }
+// }
