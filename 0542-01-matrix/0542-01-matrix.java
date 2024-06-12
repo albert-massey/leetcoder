d1c4654e94bb002396ecf816b94c7e71b343c066
Albert Massey <eralbert9191@gmail.com>
2024-06-12 14:30:50 -0700

    Time: 9 ms (91.06%), Space: 47.6 MB (27.63%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -13,7 +13,7 @@ class Solution {
         for(int i = 0; i<matrix.length; i++)
             for(int j = 0; j<matrix[0].length; j++)
                 if(matrix[i][j]==1)
-                    dfs(matrix, i, j, -11);
+                    dfs(matrix, i, j, -1);
         
         return matrix;
     }
