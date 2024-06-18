4c44791459c8fa5f522f7fb42d48b4ed56c6081d
Albert Massey <eralbert9191@gmail.com>
2024-06-18 15:56:07 -0700

    Time: 2 ms (85.48%), Space: 43.8 MB (99.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,54 @@
 class Solution {
+    List<List<Integer>> result;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        helper(candidates, target, 0, new ArrayList<>(), result);
+        result = new ArrayList<>();
+        helper(candidates, target, 0, new ArrayList<>());
         return result;
         
     }
-    private void helper(int[] candidates, int target, int idx, List<Integer> path, List<List<Integer>> result) {
+    
+    private void helper(int[] candidates, int target, int pivot, List<Integer> path) {
         //base
         if(target == 0) {
             result.add(new ArrayList<>(path));
             return;
         }
-        if(target < 0 || idx == candidates.length) return;
+        if(target < 0) return;
         
         //logic
-        
-        path.add(candidates[idx]);
-        helper(candidates, target-candidates[idx], idx, path, result);
-        path.remove(path.size()-1);
-        helper(candidates, target, idx+1, path, result);
+        for(int i = pivot; i < candidates.length; i++) {
+            
+            path.add(candidates[i]);
+            helper(candidates, target-candidates[i], i, path);
+            path.remove(path.size()-1);
+
+        }
+
         
     }
+    
+    
+    
+    
+    
+    
+    
+    
+//     private void helper(int[] candidates, int target, int idx, List<Integer> path, List<List<Integer>> result) {
+//         //base
+//         if(target == 0) {
+//             result.add(new ArrayList<>(path));
+//             return;
+//         }
+//         if(target < 0 || idx == candidates.length) return;
+        
+//         //logic
+//         path.add(candidates[idx]);
+//         helper(candidates, target-candidates[idx], idx, path, result);
+//         path.remove(path.size()-1);
+//         helper(candidates, target, idx+1, path, result);
+        
+//     }
     
     
     
