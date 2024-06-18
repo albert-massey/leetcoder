0a900e7c67f6db2ba3daecd1e94d8f14b709a804
Albert Massey <eralbert9191@gmail.com>
2024-06-18 13:51:43 -0700

    Time: 2 ms (85.48%), Space: 43.9 MB (98.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -6,19 +6,70 @@ class Solution {
         return result;
         
     }
-    
-    private void helper(int[] candidates, int target, int pivot, List<Integer> path) {
+    private void helper(int[] candidates, int target, int idx, List<Integer> path) {
         //base
-        if(target < 0 || pivot == candidates.length) return;
         if(target == 0) {
             result.add(new ArrayList<>(path));
             return;
         }
+        if(target < 0 || idx == candidates.length) return;
         
-        for(int i = pivot; i < candidates.length; i++) {
-            path.add(candidates[i]);
-            helper(candidates, target - candidates[i], i, path);
-            path.remove(path.size() -1);
-        }
+        //logic
+        helper(candidates, target, idx+1, path);
+        
+        path.add(candidates[idx]);
+        helper(candidates, target-candidates[idx], idx, path);
+        path.remove(path.size()-1);
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     private void helper(int[] candidates, int target, int pivot, List<Integer> path) {
+//         //base
+//         if(target < 0 || pivot == candidates.length) return;
+//         if(target == 0) {
+//             result.add(new ArrayList<>(path));
+//             return;
+//         }
+        
+//         for(int i = pivot; i < candidates.length; i++) {
+//             path.add(candidates[i]);
+//             helper(candidates, target - candidates[i], i, path);
+//             path.remove(path.size() -1);
+//         }
+//     }
 }
