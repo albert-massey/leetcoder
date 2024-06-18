c0fbe528db3c962582d9113e2e62a604a549a562
Albert Massey <eralbert9191@gmail.com>
2024-06-18 15:57:05 -0700

    Time: 2 ms (85.48%), Space: 44.5 MB (41.33%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -6,7 +6,6 @@ class Solution {
         return result;
         
     }
-    
     private void helper(int[] candidates, int target, int pivot, List<Integer> path) {
         //base
         if(target == 0) {
@@ -21,82 +20,6 @@ class Solution {
             path.add(candidates[i]);
             helper(candidates, target-candidates[i], i, path);
             path.remove(path.size()-1);
-
         }
-
-        
     }
-    
-    
-    
-    
-    
-    
-    
-    
-//     private void helper(int[] candidates, int target, int idx, List<Integer> path, List<List<Integer>> result) {
-//         //base
-//         if(target == 0) {
-//             result.add(new ArrayList<>(path));
-//             return;
-//         }
-//         if(target < 0 || idx == candidates.length) return;
-        
-//         //logic
-//         path.add(candidates[idx]);
-//         helper(candidates, target-candidates[idx], idx, path, result);
-//         path.remove(path.size()-1);
-//         helper(candidates, target, idx+1, path, result);
-        
-//     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//     private void helper(int[] candidates, int target, int pivot, List<Integer> path) {
-//         //base
-//         if(target < 0 || pivot == candidates.length) return;
-//         if(target == 0) {
-//             result.add(new ArrayList<>(path));
-//             return;
-//         }
-        
-//         for(int i = pivot; i < candidates.length; i++) {
-//             path.add(candidates[i]);
-//             helper(candidates, target - candidates[i], i, path);
-//             path.remove(path.size() -1);
-//         }
-//     }
 }
