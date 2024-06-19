87d3e1742f327269bab037cd711db09ad6c6f909
Albert Massey <eralbert9191@gmail.com>
2024-06-19 13:19:11 -0700

    Time: 1 ms (52.31%), Space: 42.4 MB (70.45%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,12 +1,12 @@
 class Solution {
-    List<List<Integer>> result;
+    // List<List<Integer>> result;
     public List<List<Integer>> subsets(int[] nums) {
-        result = new ArrayList<>();
-        helper(nums, 0, new ArrayList<>());
+        List<List<Integer>> result = new ArrayList<>();
+        helper(nums, 0, new ArrayList<>(), result);
         return result;
     }
     
-    private void helper(int[] nums, int pivot, List<Integer> path) {
+    private void helper(int[] nums, int pivot, List<Integer> path, List<List<Integer>> result) {
         //base
         if(pivot == nums.length) {
             result.add(new ArrayList<>(path));
@@ -17,7 +17,7 @@ class Solution {
         result.add(new ArrayList<>(path));
         for(int i = pivot; i < nums.length; i++) {
             path.add(nums[i]);
-            helper(nums, i+1, path);
+            helper(nums, i+1, path, result);
             path.remove(path.size()-1);
         }
     }
