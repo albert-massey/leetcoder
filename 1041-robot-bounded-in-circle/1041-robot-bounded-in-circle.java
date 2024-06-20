3b117fa95846c8b991ca831f80716e6a61a450c0
Albert Massey <eralbert9191@gmail.com>
2024-06-20 15:38:05 -0700

    Time: 0 ms (100.00%), Space: 41.7 MB (8.11%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
@@ -1,26 +1,23 @@
 class Solution {
     public boolean isRobotBounded(String instructions) {
-        // north = 0, east = 1, south = 2, west = 3
-        int[][] directions = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        // Initial position is in the center
-        int x = 0, y = 0;
-        // facing north
+        HashSet<int[]> set = new HashSet<>();
+        //               north    east    south     west
+        int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        //                idx
         int idx = 0;
-        
-        for (char i : instructions.toCharArray()) {
-            if (i == 'L')
+        int x = 0;
+        int y = 0;
+        char[] insArr = instructions.toCharArray(); 
+        for(char c: insArr) {
+            if(c == 'G') {
+                x += dirs[idx][0];
+                y += dirs[idx][1];
+            } else if(c == 'L') {
                 idx = (idx + 3) % 4;
-            else if (i == 'R')
+            } else if(c == 'R') {
                 idx = (idx + 1) % 4;
-            else {
-                x += directions[idx][0];
-                y += directions[idx][1];   
-            }    
+            }
         }
-        
-        // after one cycle:
-        // robot returns into initial position
-        // or robot doesn't face north
-        return (x == 0 && y == 0) || (idx != 0);
+        return (x == 0 && y == 0) || idx != 0;
     }
 }
