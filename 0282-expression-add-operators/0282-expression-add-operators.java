db9d12476944c20edc6da85f7b46da8888e4dfad
Albert Massey <eralbert9191@gmail.com>
2024-06-28 17:48:35 -0700

    Time: 64 ms (85.46%), Space: 45.3 MB (43.91%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -2,15 +2,15 @@ class Solution {
     List<String> result;
     public List<String> addOperators(String num, int target) {
         this.result = new ArrayList<>();
-        helper(num, 0, target, "", 0, 0);
+        helper(num, 0, target, new StringBuilder(), 0, 0);
         return result;
     }
     
-    private void helper(String num, int pivot, int target, String path, long calc, long tail) {
+    private void helper(String num, int pivot, int target, StringBuilder path, long calc, long tail) {
         //base
         if(pivot == num.length()) {
             if(calc == target) {
-                result.add(path);
+                result.add(path.toString());
             }
             return;
         }
@@ -20,16 +20,30 @@ class Solution {
             //preceding zero
             if(num.charAt(pivot) == '0' && pivot != i) continue;
             long curr = Long.parseLong(num.substring(pivot, i+ 1));
-            
+            int lenP = path.length();
             if(pivot == 0) {
-                helper(num, i+1, target, path+curr, curr, curr);
+                path.append(curr);
+                helper(num, i+1, target, path, curr, curr);
+                path.setLength(lenP);
             } else {
-                //+
-                helper(num, i+1, target, path+"+"+curr, calc + curr, curr);
+                //+                
+                path.append('+');
+                path.append(curr);
+                helper(num, i+1, target, path, calc + curr, curr);
+                path.setLength(lenP);
+
                 //-
-                helper(num, i+1, target, path+"-"+curr, calc - curr, -curr);
+                path.append('-');
+                path.append(curr);
+                helper(num, i+1, target, path, calc - curr, -curr);
+                path.setLength(lenP);
+
                 //*
-                helper(num, i+1, target, path+"*"+curr, calc -tail + tail * curr, tail * curr);
+                path.append('*');
+                path.append(curr);
+                helper(num, i+1, target, path, calc -tail + tail * curr, tail * curr);
+                path.setLength(lenP);
+
             }
         }
         
