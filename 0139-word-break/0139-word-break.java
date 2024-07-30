414b9c1306ffd2fb53728621006f66e613a506b5
Albert Massey <eralbert9191@gmail.com>
2024-07-30 15:32:44 -0700

    Time: 10 ms (17.25%), Space: 45 MB (6.51%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>();
+        for(String str: wordDict) {
+            set.add(str);
+        }
+        int n = s.length();
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                String sub = s.substring(j, i);
+                if(set.contains(sub) && dp[j]== true) {
+                    dp[i] = true;
+                    break;
+                } else {
+                    
+                    dp[i] = false;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
