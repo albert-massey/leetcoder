17a594899c67e454abedc6706a2f937e1e242f7c
Albert Massey <eralbert9191@gmail.com>
2024-07-31 21:49:00 -0700

    Time: 0 ms (100.00%), Space: 40.6 MB (15.05%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -5,10 +5,15 @@ class Solution {
     }
     
     private int helper(int r, int c, int m, int n, int[][] mat) {
-        if(r > m-1 || c > n-1) return 0;
-        if(r == m-1 && c == n-1) return 1;
-        if(mat[r][c] != 0) return mat[r][c];
-        
+        if(r > m-1 || c > n-1) {
+            return 0;
+        }
+        if(r == m-1 && c == n-1) {
+            return 1;
+        }
+        if(mat[r][c] != 0) {
+            return mat[r][c];
+        }
         
         //logic
         int right = helper(r, c+1, m,n,mat);
