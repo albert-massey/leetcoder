aca3ea6c909f97758e872d786571d71d2b09eb59
Albert Massey <eralbert9191@gmail.com>
2024-12-08 13:39:08 -0800

    Time: 3 ms (34.63%), Space: 44.4 MB (40.64%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -24,14 +24,10 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         if(root == null) return root;
-        
         Queue<Node> q = new LinkedList<>();
-        
         q.add(root);
-        
         while(!q.isEmpty()) {
             int size = q.size();
-            
             for(int i = 0; i < size; i++) {
                 Node curr = q.poll();
                 if(i != size - 1) {
@@ -43,14 +39,8 @@ class Solution {
                 if(curr.right != null) {
                     q.add(curr.right);
                 }
-                
-                
             }
-            
-            
-            
         }
         return root;
-        
     }
 }
